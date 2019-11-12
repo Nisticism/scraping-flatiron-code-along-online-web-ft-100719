f88e858ead42dacc29664dc50dbf674a733757be
Hans Foster <fosterhans@gmail.com>
2019-11-12 07:33:34 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,9 +2,15 @@ class Course
   
   attr_accessor :title, :schedule, :description
   
+  @@all = []
+  
   def initalize
     @@all << self 
-    
+  end
+  
+  def self.all
+    @@all
+  end
     
   
 end
