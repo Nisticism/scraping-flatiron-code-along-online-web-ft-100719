a9207ca92288f468367cf4e4c78ad25c3a86f9fd
Hans Foster <fosterhans@gmail.com>
2019-11-12 08:01:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,12 @@ class Scraper
     end
   end
   
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+  
 end
 
 
